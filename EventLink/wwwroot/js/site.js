ab213d3eafef656fe29daba6a25f1dfca8620c78
Matthew Cordas <cordasmd@mail.uc.edu>
2023-11-01 14:23:21 -0400

    Dark Mode css addition dark mode layout page page changes.

--- EventLink/wwwroot/js/site.js
+++ EventLink/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+<script>
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const body = document.body;
+
+    darkModeToggle.addEventListener("click", function () {
+        body.classList.toggle("dark-mode")});
+</script>
