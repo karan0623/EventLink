2e1ef4b0722e02175d59ad06ac6a481eec955157
Karan Patel <patel3k7@mail.uc.edu>
2023-11-08 15:14:18 -0500

    Deleted everything related to pulling information for the API on the Web App since this is being handled by the console application.

--- EventLink/Controllers/HomeController.cs
+++ EventLink/Controllers/HomeController.cs
@@ -16,13 +16,6 @@ namespace EventLink.Controllers
 
         public IActionResult Index()
         {
-            using (var webClient = new WebClient())
-            {
-
-                string jsonString = webClient.DownloadString("https://api.apify.com/v2/actor-tasks/exclusive_commode~eventlink---instagram-post-scraper/run-sync-get-dataset-items?token=apify_api_Uea6k2FqNtwergHaVTQ5YiVQm2Q4d80BNqpH");
-                var post = InstagramPosts.FromJson(jsonString);
-                ViewData["Insta"] = post;
-            }
             return View();
         }
 
