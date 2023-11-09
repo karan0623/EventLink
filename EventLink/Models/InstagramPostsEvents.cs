72a3dde32891e83be1f12e3ab927784b3d311dc3
Karan Patel <patel3k7@mail.uc.edu>
2023-11-09 12:19:03 -0500

    Created InstagramPostsEvents class

---
+++ EventLink/Models/InstagramPostsEvents.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace EventLink.Models
+{
+    public class InstagramPostsEvents
+    {
+        
+        public string Id { get; set; }
+
+        
+        public string Caption { get; set; }
+
+        
+        public Uri DisplayUrl { get; set; }
+
+        
+        public Uri VideoUrl { get; set; }
+
+        
+        public DateTimeOffset? Timestamp { get; set; }
+
+        
+        public string OwnerFullName { get; set; }
+
+        
+        public string OwnerUsername { get; set; }
+
+        
+        public long? OwnerId { get; set; }
+
+        
+        public string LocationName { get; set; }
+
+    }
+}
