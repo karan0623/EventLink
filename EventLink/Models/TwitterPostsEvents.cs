23663bccd0c3d104953b3b8f8915181ad3abb7cb
Matthew Cordas <cordasmd@mail.uc.edu>
2024-03-07 12:26:30 -0500

    Updating Filter Functionality

--- EventLink/Models/TwitterPostsEvents.cs
+++ EventLink/Models/TwitterPostsEvents.cs
@@ -6,7 +6,7 @@
 
         public string? FullText { get; set; }
 
-        public string Name { get; set; }
+        public string User { get; set; }
 
         public Uri? Url { get; set; }
         public string Category { get; set; }
